d700ef95b75c46a6456f4a2329089b9e89bd22e7
Alex <alwx162@gmail.com>
2024-05-09 11:27:26 -0500

    validacion

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const Note = require('./models/note')
-const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
+
 
 app.use(cors())
 app.use(express.json())
@@ -21,7 +21,7 @@ app.get('/api/notes', (request, response) => {
 
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   /**const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
   if (note) {
@@ -29,53 +29,81 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }*/
-  Note.findById(request.params.id).then(note=>{
-    if(note){
-       response.json(note)
-    }else{
+  Note.findById(request.params.id).then(note => {
+    if (note) {
+      response.json(note)
+    } else {
       response.status(404).end()
     }
-  }).catch(error =>{
-    console.log(error)
-    response.status(500).end()
-  })
+  }).catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body;
 
-  response.status(204).end()
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' })
+    .then(result => {
+      response.json(result)
+    }).catch(error => next(error))
 })
+
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end()
+
+  }).catch(error => next(error))
+
+})
+
 
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
   return maxId + 1;
 }
 
-app.post('/api/notes', (request, response) => {
+
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
-  
-  if (!body.content) {
+
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing"
     })
   }
-
   const note = new Note({
     content: body.content,
     important: body.important || false
   })
   note.save().then(savedNote => {
     response.json(savedNote)
-  })
+  }).catch(error => next(error))
 })
 
+
+//unknown endpoint sirve para manejar los errores de rutas no encontradas
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/note.js
+++ models/note.js
@@ -18,7 +18,11 @@ mongoose.connect(url)
   })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 4,
+    required: true,
+  },
   important: Boolean,
 })
 
@@ -29,5 +33,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
- 
+
 module.exports = mongoose.model('Note', noteSchema)
